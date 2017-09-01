4888a4efed9ecd77f25f520fb678cc100bcc8461
Piotr Kedra <piotr.kedra@schibsted.pl>
2017-09-01 15:31:06 +0200

    Add exercise 04

---
+++ learnyounode/04-my_first_async_io.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+    if (err)
+        return console.error(err);
+    else {
+        var lines = data.split('\n').length - 1
+        console.log(lines);
+    }
+});
