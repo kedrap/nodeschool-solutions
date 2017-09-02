01c80a48127e420d78aedddefb5ac29313358b47
Piotr Kedra <piotr.kedra@schibsted.pl>
2017-09-02 22:10:56 +0200

    Add exercise 06

--- js-best-practices/changeHandler.js
+++ js-best-practices/changeHandler.js
@@ -1,23 +1,39 @@
+// COINS:
+// [p]enny
+// [n]ickel
+// [d]ime
+// [q]uarter
+let coins = {
+    'p': 1,
+    'n': 5,
+    'd': 10,
+    'q': 25,
+}
+
 module.exports = {
     getAmount: function (coinType) {
-        // COINS:
-        // [p]enny
-        // [n]ickel
-        // [d]ime
-        // [q]uarter
-        let types = {
-            'p': 1,
-            'n': 5,
-            'd': 10,
-            'q': 25,
-        }
 
-        if (!types.hasOwnProperty(coinType)) {
+        if (!coins.hasOwnProperty(coinType)) {
             throw new Error('Unrecognized coin ' + coinType);
         }
 
-        console.log(types, coinType);
+        console.log(coins, coinType);
 
-        return types[coinType];
+        return coins[coinType];
     },
+
+    convertToChange: function (amount) {
+        let change = [];
+        let coinsByValue = Object.keys(coins).reverse();
+
+        for (let name of coinsByValue) {
+            let coin = coins[name];
+            while (amount >= coin) {
+                change.push(name);
+                amount -= coin;
+            }
+        }
+
+        return change;
+    }
 };
